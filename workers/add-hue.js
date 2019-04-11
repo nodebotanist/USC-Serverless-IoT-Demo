6ef5c1f2c7515de6eaa084aec208b3bc9823d68e
nodebotanist <the@nodebotanist>
2019-04-11 15:21:50 -0500

    add-hue does what it is supposed to do

--- workers/add-hue.js
+++ workers/add-hue.js
@@ -1,8 +1,23 @@
+const color = require('color')
+
 addEventListener("fetch", event => {
   event.respondWith(fetchAndApply(event.request));
 });
 
 async function fetchAndApply(request) {
 	let formData = await request.formData()
-  return new Response(JSON.stringify(formData.get('color')));
+	let parsedColor
+	try {
+		parsedColor = color(formData.get('color'))
+	} catch (err) {
+		return new Response(err.message)
+	}
+	let colors = await HUES.get('colors')
+	colors = JSON.parse(colors) || []
+	colors.push(parsedColor.color)
+	if(colors.length > 28){
+		colors = colors.slice(colors.length - 27 , 28)
+	}
+	await HUES.put('colors', JSON.stringify(colors))
+  return new Response(JSON.stringify(colors))
 }
